3ac94ec4924e3d5cc6a7bf34d08fcb825fa024f4
Alicia Appelhagen <uoxyc@student.kit.edu>
2023-03-31 09:55:21 +0200

    Apply background SMT runner on settings changes, after auto mode and after loading a proof

--- keyext.backgroundSMT/src/main/java/de/uka/ilkd/key/kexext/backgroundSMT/BackgroundSolverRunner.java
+++ keyext.backgroundSMT/src/main/java/de/uka/ilkd/key/kexext/backgroundSMT/BackgroundSolverRunner.java
@@ -20,7 +20,7 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BackgroundSolverRunner.class);
 
-    private final List<SolverType> solverTypes = new ArrayList<>();
+    private final Set<SolverType> solverTypes = new HashSet<>();
     private final Proof proof;
     private final KeYMediator mediator;
     private final List<BackgroundSMTProblem> problems = new ArrayList<>();
@@ -31,9 +31,11 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
 
     private final SMTSettings settings;
 
+    private final BackgroundSMTExtension extension;
+
     private boolean waitForAutomodeFinished = false;
 
-    private final BackgroundSMTExtension extension;
+    private double timeout;
 
     public BackgroundSolverRunner(BackgroundSMTExtension extension, Proof proof, KeYMediator mediator) {
         this.extension = extension;
@@ -41,24 +43,10 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
         this.proof.addRuleAppListener(this);
         this.mediator = mediator;
         this.mediator.getUI().getProofControl().addAutoModeListener(this);
-
-        // TODO set background solverTypes via settings
-        this.solverTypes.addAll(
-            SolverTypes.getSolverTypes().stream().filter(t -> t.getName().equals("Z3")).map(t -> t.copy()).collect(Collectors.toList()));
-        // Don't use the counter example generator
-        // (generally, using the SMTObjectTranslator causes exceptions during translation)
-        solverTypes.remove(SolverTypes.Z3_CE_SOLVER);
-
-    for (SolverType type : solverTypes) {
-            // TODO avoid magic number
-            // set timeout to 1.5 seconds
-            type.setSolverTimeout(1500);
-        }
-
         settings = new DefaultSMTSettings(proof.getSettings().getSMTSettings(),
                 ProofIndependentSettings.DEFAULT_INSTANCE.getSMTSettings(),
                 proof.getSettings().getNewSMTSettings(), proof);
-
+        applyTo(proof.getSubtreeGoals(proof.root()).toList());
     }
 
     /*public Set<SMTProblem> getSolvedProblems() {
@@ -88,28 +76,39 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
         if (e.getSource() != proof || waitForAutomodeFinished) {
             return;
         }
-        // Find the new goals/SMT problems resulting from the rule application.
-        Collection<SMTProblem> newProblems = new ArrayList<>();
         // e.getNewGoals() should not be null as we are in the ruleApplied method(?)
         if (e.getNewGoals() == null) {
             return;
         }
+        applyTo(e.getNewGoals().toList());
+    }
+
+    private void applyTo(Collection<Goal> goals) {
+        // Find the new goals/SMT problems resulting from the rule application.
+        Collection<SMTProblem> newProblems = new ArrayList<>();
         // Get the new goals/SMT problems resulting from the rule application.
-        for (Goal goal : e.getNewGoals()) {
+        for (Goal goal : goals) {
             SMTProblem problem = new SMTProblem(goal);
             newProblems.add(problem);
         }
+        if (solverTypes.isEmpty()) {
+            return;
+        }
+        launch(newProblems);
+    }
+
+    private void launch(Collection<SMTProblem> smtProblems) {
         // Launch the background SMT solvers on the new SMT problems.
         // TODO solver instances are never killed (should this be handled here or in the SolverLauncher?)
         // TODO handle SMTTranslator exceptions here?
         SolverLauncher launcher = new SolverLauncher(settings);
         launchers.add(launcher);
-        for (SMTProblem problem : newProblems) {
+        for (SMTProblem problem : smtProblems) {
             problems.add(new BackgroundSMTProblem(problem, this, launcher));
         }
         launcher.addListener(this);
         Thread thread = new Thread(() -> {
-            launcher.launch(solverTypes, newProblems, proof.getServices());
+            launcher.launch(solverTypes, smtProblems, proof.getServices());
         }, "BackgroundSMT");
         threads.add(thread);
         thread.start();
@@ -128,7 +127,7 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
     @Override
     public void autoModeStopped(ProofEvent e) {
         waitForAutomodeFinished = false;
-        ruleApplied(e);
+        applyTo(proof.openGoals().toList());
     }
 
 
@@ -141,6 +140,7 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
     @Override
     public void launcherStopped(SolverLauncher launcher, Collection<SMTSolver> finishedSolvers) {
         launchers.remove(launcher);
+        extension.refresh();
     }
 
     @Override
@@ -152,9 +152,32 @@ public class BackgroundSolverRunner implements SolverLauncherListener, RuleAppLi
     public Optional<SMTSolverResult> getCachedResult(Sequent sequent) {
         for (BackgroundSMTProblem problem : problems) {
             if (problem.getProblem().getSequent().equals(sequent)) {
-                return Optional.of(problem.getProblem().getFinalResult());
+                SMTSolverResult result = problem.getProblem().getFinalResult();
+                if (result.isValid() != SMTSolverResult.ThreeValuedTruth.UNKNOWN) {
+                    return Optional.of(problem.getProblem().getFinalResult());
+                }
             }
         }
         return Optional.empty();
+    }
+
+    public void setTimeout(double timeout) {
+        this.timeout = timeout;
+    }
+
+    public void setSolvers(Collection<SolverType> checkedTypes) {
+        this.solverTypes.clear();
+        this.solverTypes.addAll(checkedTypes);
+        tryForCurrentlyNotRunningGoals();
+    }
+
+    private void tryForCurrentlyNotRunningGoals() {
+        List<SMTProblem> inactiveProblems = new ArrayList<>();
+        for (Goal goal : proof.openGoals()) {
+            if (getCachedResult(goal.sequent()).isEmpty()) {
+                inactiveProblems.add(new SMTProblem(goal));
+            }
+        }
+        launch(inactiveProblems);
     }
 }
